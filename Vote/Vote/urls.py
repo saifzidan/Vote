fc9a3691807ead39c02479103d9dbf2ff97d7bb6
Saif Zidan <saiflion10000@gmail.com>
2023-08-22 05:54:46 +0300

    setting up homepage

--- Vote/Vote/urls.py
+++ Vote/Vote/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,5 @@ urlpatterns = [
     path('Worker/', include('Worker.urls')),
     path('Result/', include('Result.urls')),
     path('result/', include('Result.urls')),
+    path('', views.index, name='index'),
 ]
